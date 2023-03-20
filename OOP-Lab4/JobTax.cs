3130781398809c0dbb0617043f1756f89883810f
Oleksii Kurin <olexkurin@gmail.com>
2023-03-21 00:42:15 +0200

    Add taxes hierarchy and tests

---
+++ OOP-Lab4/JobTax.cs
@@ -0,0 +1,28 @@
+﻿namespace OOP_Lab4;
+
+public class JobTax : Tax
+{
+	private const double _maxBenefitedAmount = 2684.0;
+	private const double _benefitAmount = 0.5 * _maxBenefitedAmount;
+
+	public JobTax(TaxpayerData incomeData) : base(incomeData)
+	{
+	}
+
+	protected override double Percent => 15.0;
+	public override string Name => "Job";
+
+	protected override double GetTaxedAmount(TaxpayerData taxpayerData)
+	{
+		double amount = taxpayerData.MainJobIncome + taxpayerData.AdditionalJobIncome;
+		if (amount <= _maxBenefitedAmount)
+		{
+			amount -= _benefitAmount;
+		}
+		if (taxpayerData.ChildrenCount > 1)
+		{
+			amount -= _benefitAmount * taxpayerData.ChildrenCount;
+		}
+		return amount;
+	}
+}
--- OOP-Lab4/Program.cs
+++ OOP-Lab4/Program.cs
@@ -1,10 +1,44 @@
-﻿namespace OOP_Lab4
+﻿namespace OOP_Lab4;
+
+public static class Program
 {
-	internal class Program
+	static readonly Random rand = new();
+	static double NextDouble(double max) => rand.NextDouble() * max;
+	static void WriteArray<T>(T[] arr) => Array.ForEach(arr, i => Console.WriteLine($"-> {i}"));
+	static TaxpayerData GenerateTaxpayerData() =>
+		new(
+			MainJobIncome: NextDouble(50000.0),
+			AdditionalJobIncome: NextDouble(30000.0),
+			AuthorAwardsAmount: NextDouble(10000.0),
+			GoodsSellsIncome: NextDouble(20000.0),
+			MoneyGiftsAmount: NextDouble(10000.0),
+			GoodsGiftsAmount: NextDouble(15000.0),
+			AbroadTransfersAmount: NextDouble(40000.0),
+			ChildrenCount: rand.Next(10)
+		);
+
+	static void Main()
 	{
-		static void Main(string[] args)
+		var taxpayerData = GenerateTaxpayerData();
+		Console.WriteLine(taxpayerData);
+
+		var taxes = new Tax[]
 		{
-			Console.WriteLine("Hello, World!");
-		}
+			new AbroadTransfersTax(taxpayerData),
+			new GiftsTax(taxpayerData),
+			new GoodsTax(taxpayerData),
+			new JobTax(taxpayerData),
+			new MilitaryTax(taxpayerData)
+		};
+
+		Console.WriteLine("Taxes array before sort:");
+		WriteArray(taxes);
+		Console.WriteLine();
+
+		Console.WriteLine("Taxes array after sort:");
+		Array.Sort(taxes);
+		WriteArray(taxes);
+
+		Console.ReadLine();
 	}
 }
